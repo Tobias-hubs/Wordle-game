80568da945c00ca4af7339f2f0013cc642eb47d3
Tobias Larsson <tobbe-larsson@hotmail.com>
2025-03-28 15:38:02 +0100

    WIP: Structure gameLogic.js on backend and game.js on frontend

---
+++ backend/game/gameLogic.js
@@ -0,0 +1,50 @@
+//Code from previous project Testade-algorithmer
+
+
+export function controllGuess(guess, correctWord) { 
+    let feedback = [];
+    
+    for(let i = 0; i < guess.length; i++)  {
+        let letter = guess[i];
+
+        if (letter === correctWord[i]) {
+            feedback.push({letter, result: "correct"});
+        } else { 
+            feedback.push({letter, result: null });
+        }
+    }
+
+    for (let i = 0; i < guess.length; i++) {
+        if(feedback[i].result === null) {
+            const letter = guess[i];
+            if (correctWord.includes(letter)) {
+                feedback[i].result = "missplaced";
+            } else {
+                feedback[i].result = "incorrect";
+        }
+    } 
+    }   
+
+    return feedback; 
+
+   
+}
+
+export function chooseWord(wordList, length, specialLetters)   {
+
+    let validWords = wordList.filter(word => word.length === length);
+    
+   
+    if (validWords.length === 0) {
+        return `There are no words with ${length} letters`; 
+    }
+
+    if (!specialLetters) {
+       validWords = validWords.filter(word => new Set(word).size === word.length);
+        
+    }
+    
+    const randomIndex = Math.floor(Math.random() * validWords.length);
+        return validWords[randomIndex];
+    
+}
---
+++ client/src/Game.js
@@ -0,0 +1,10 @@
+import {useState} from react; 
+
+function Game() { 
+const [guess, setGuess] = useState("");
+const [feedback, setFeedback] = useState("");
+const [gameover, setGameover] = useState(false);
+}
+
+const correctWord = "hello";
+
